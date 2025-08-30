8ec65b234c7ed7af0bbda91ddbad160592b8ad09
Ttamires <84874204+Ttamires@users.noreply.github.com>
2025-08-30 16:33:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from comunidadeimpressionadora import app
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
