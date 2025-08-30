9ec84b7e39341bfbb8184cde772c5aa2f9031c91
Ttamires <84874204+Ttamires@users.noreply.github.com>
2025-08-30 15:53:23 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -6,11 +6,13 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', '29cecf8afd6176f06bb3f55472d490d1')
+app.config['DEBUG'] = False 
 if os.getenv('DATABASE_URL'):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -19,5 +21,6 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
 from comunidadeimpressionadora import routes
+
 
 
